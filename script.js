d4530d8149a74c69b9b0c7c2e3dfcb4201e5c8b1
rohit19060 <rohitjain19060@gmail.com>
2025-11-30 07:46:56 +0530

    Add passkey creation demo with WebAuthn
    Introduces index.html with a button to create a new passkey and script.js implementing passkey creation using WebAuthn APIs. Example credential options are hardcoded for demonstration purposes.

---
+++ script.js
@@ -0,0 +1,70 @@
+const responseFromServer =
+  // Example `PublicKeyCredentialCreationOptions` contents
+  {
+    challenge: "dGVzdGRk",
+    rp: {
+      name: "Example",
+      id: "example.com",
+    },
+    user: {
+      id: "dGVzdGRk",
+      name: "john78",
+      displayName: "John",
+    },
+    pubKeyCredParams: [
+      {
+        alg: -7,
+        type: "public-key",
+      },
+      {
+        alg: -257,
+        type: "public-key",
+      },
+    ],
+    excludeCredentials: [
+      {
+        id: "dGVzdGRk",
+        type: "public-key",
+        transports: ["internal"],
+      },
+    ],
+    authenticatorSelection: {
+      authenticatorAttachment: "platform",
+      requireResidentKey: true,
+    },
+  };
+
+function createPasskey() {
+  // Availability of `window.PublicKeyCredential` means WebAuthn is usable.
+  // `isUserVerifyingPlatformAuthenticatorAvailable` means the feature detection is usable.
+  // `isConditionalMediationAvailable` means the feature detection is usable.
+  if (
+    window.PublicKeyCredential &&
+    PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable &&
+    PublicKeyCredential.isConditionalMediationAvailable
+  ) {
+    // Check if user verifying platform authenticator is available.
+    Promise.all([
+      PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable(),
+      PublicKeyCredential.isConditionalMediationAvailable(),
+    ]).then(async (results) => {
+      if (results.every((r) => r === true)) {
+        // Display "Create a new passkey" button
+        // Fetch an encoded `PubicKeyCredentialCreationOptions` from the server.
+        // const _options = await fetch("/webauthn/registerRequest");
+        // const _options = responseFromServer;
+
+        // Deserialize and decode the `PublicKeyCredentialCreationOptions`.
+        // const decoded_options = JSON.parse(_options);
+        const options =
+          PublicKeyCredential.parseCreationOptionsFromJSON(responseFromServer);
+        const credential = await navigator.credentials.create({
+          publicKey: options,
+        });
+        console.log(credential);
+      }
+    });
+  }
+}
+
+createPasskey();
